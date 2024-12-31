be5d5d563bddfe476dd35ad93fb90f64eb961149
Akyoo501 <143004721+Akyoo501@users.noreply.github.com>
2024-12-31 23:24:17 +0300

    Add files via upload

---
+++ Welcome   2025.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<endl<<endl;
+    cout<<" * * * * HAPPY NEW YEAR TO ALL * * * * ";
+    cout<<endl<<endl;
+
+cout<<"  @ @ @ @            @ @ @ @    "<<endl;
+cout<<" @        @        @         @  "<<endl;
+cout<<" @         @      @          @  "<<endl;
+cout<<"  @          @   @          @   "<<endl;
+cout<<"   @          @@@          @    "<<endl;
+cout<<"    @                     @     "<<endl;
+cout<<"     @                   @      "<<endl;
+cout<<"      @      2025       @       "<<endl;
+cout<<"       @               @        "<<endl;
+cout<<"        @             @         "<<endl;
+cout<<"         @           @          "<<endl;
+cout<<"          @         @           "<<endl;
+cout<<"           @       @            "<<endl;
+cout<<"            @     @             "<<endl;
+cout<<"             @   @              "<<endl;
+cout<<"              @@@               "<<endl;
+
+
+cout<<endl<<endl;
+
+return 0;
+
+}
